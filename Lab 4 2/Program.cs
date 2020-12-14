aec621d4d1e5b419d645ccf476606c8001a375cc
ilyadery <ilyadrbn@gmail.com>
2020-12-14 13:12:22 +0200

    Добавьте файлы проекта.

---
+++ Lab 4 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Введите значение а: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Результат поразрядной операции И равен: ");
+            Console.Write("Для десятичной: ");
+            Console.Write(a & b);    
+        }
+    }
+}
